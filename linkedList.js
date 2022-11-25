ef3889a94404d08b3fb521449c4207d5fca579ea
Donavyn <donavynpence@yahoo.com>
2022-11-25 12:23:50 -0700

    project requirements are complete, could be better but moving on

--- linkedList.js
+++ linkedList.js
@@ -38,13 +38,13 @@ const LinkedList = () => {
     }
     
     const getListSize = () => {
+      if (Head === null) {
+        return 0;
+      }
 
     let listItem = Head;
     let counter = 1;
       
-      if (Head === null) {
-        return 0;
-      }
 
       while(listItem.getNext() !== null) {
         counter++;
@@ -55,11 +55,11 @@ const LinkedList = () => {
     }
 
     const getTail = () => {
-      let listItem = Head;
-
       if(Head === null ) {
         return 'List is empty'
        }
+      let listItem = Head;
+
 
       while(listItem.getNext() !== null) {
         listItem = listItem.getNext();
@@ -68,12 +68,12 @@ const LinkedList = () => {
     }
 
     const atPosition = (value) => {
-      let listItem = Head;
-      let counter = value;
-
       if (Head === null) {
         return 'List is empty'
       }
+      let listItem = Head;
+      let counter = value;
+
 
       while(counter > 1) {
         counter--;
@@ -81,23 +81,29 @@ const LinkedList = () => {
       }
       return listItem
     }
+    /* this uses the head of the list being index 1 */
 
     const pop = () => {
-      let popTarget = atPosition(getListSize() - 1)
-
       if (Head === null) {
         return 'List is empty'
       }
+      let popTarget = atPosition(getListSize() - 1)
+
       popTarget.setNext(null);
     }
+    /* does this work on a list length of 1? also need to set Head to null if
+    list is only 1 length and i pop it out actually if list is
+    length one it would be as simple as setting Head to null */
+    
+    /* this removes from the list but does not delete the node */
 
     const contains = (value) => {
+      if (Head === null) {
+        return 'List is empty'
+      }
       let listItem = Head;
       let counter = getListSize();
 
-      if (Head === null) {
-        return 'List is empty'
-      }
       while(counter > 0) {
         counter--;
         if (listItem.getValue() === value) {
@@ -110,12 +116,12 @@ const LinkedList = () => {
     }
 
     const find = (value) => {
-      let listItem = Head;
-      let counter = 0;
-
       if(Head === null ) {
         return 'List is empty'
        }
+      let listItem = Head;
+      let counter = 0;
+
        while(counter < getListSize()) {
         counter++;
         if (listItem.getValue() === value) {
@@ -126,6 +132,40 @@ const LinkedList = () => {
       return null
   }
 
+  const toString = () => {
+    if(Head === null ) {
+      return 'List is empty'
+     }
+
+    let nodeValues = [];
+    let listItem = Head;
+    let counter = 0;
+
+     while(counter < getListSize()) {
+      counter++;
+      nodeValues.push(listItem.getValue());
+      listItem = listItem.getNext();
+  }
+  nodeValues.push(null);
+  return nodeValues
+}
+
+  const insertAt = (value, index) => {
+    if(Head === null ) {
+      appendValue(value);
+      return 'List was empty so appended a value.'
+     }
+
+     const newNode = NodeCreator(value);
+     newNode.setValue(value);
+
+     let pointerToPreviousIndexLocation = atPosition(index).getNext();
+     let nodeValueBehindNewNode = atPosition(index-1);
+
+     newNode.setNext(pointerToPreviousIndexLocation);
+     nodeValueBehindNewNode.setNext(newNode);
+  }
+
     
     const getHead = () => {
      if(Head === null ) {
@@ -134,7 +174,7 @@ const LinkedList = () => {
      return Head
     };
     return{appendValue,getHead, prependValue, getListSize, getTail, atPosition
-    , pop, contains, find}
+    , pop, contains, find, toString, insertAt}
     }
     
     const NodeCreator = (inp) => {
@@ -146,13 +186,14 @@ const LinkedList = () => {
       const setNext = (inp) => next = inp;
       return {getValue, getNext, setValue, setNext}
     }
-    
-    
-    const far = LinkedList();
-/*     far.appendValue('one');
-    far.appendValue('two');
-    far.appendValue(3); */
-    
-    const ofar = LinkedList();
-    const one = NodeCreator();
-    const two = NodeCreator(12);
+    /* Will not be incorporating DRY programming rules here rather finishing
+and moving on when im done figuring everything out, not worrying
+about efficient usage but probably should have kept DRY in mind
+as i was writing rather then going back over it and altering it to BECOME 
+DRY, for now rather focusing on getting it to work rather then
+making it pretty */
+
+/* rather then doing the last extra credit which is very straightforward
+once you are done with insert version and the pop i will be moving on,
+it was a very fun informative excersize and i learned alot!
+code could be cleaner in a lot of spaces but im eager to move forward*/
--- linkedList.test.js
+++ linkedList.test.js
@@ -3,6 +3,8 @@ import {LinkedList, NodeCreator} from '../linked-list-project/linkedList';
 const appendList = LinkedList();
 const prependList = LinkedList();
 const emptyList = LinkedList();
+const extraCreditList = LinkedList();
+const otherExtraCreditList = LinkedList();
 
 test('linked properly', () => {
     expect(LinkedList);
@@ -54,14 +56,12 @@ test('returns the value in the last node of the list', () => {
 test('returns the node value at the given index', () => {
     expect(appendList.atPosition(2).getValue()).toBe(2);
 });
-/* this uses the head of the list being index 1 */
 
 test('removes the last element from the list', () => {
     expect(appendList.getTail().getValue()).toBe('four');
     appendList.pop();
     expect(appendList.getTail().getValue()).toBe(3);
 });
-/* this removes from the list but does not delete the node */
 
 test('searches for a value and returns true if found or false if not', () => {
     expect(appendList.contains(3)).toBe(true);
@@ -72,6 +72,18 @@ test('searches for a value and returns true if found or false if not', () => {
 test('returns the index of the node containing provided value, or null', () => {
     expect(appendList.find(3)).toBe(3);
     expect(appendList.find(5)).toBe(null);
+});
+
+test('prints the values of every node including the null value', () => {
+    expect(appendList.toString()).toStrictEqual(['one', 2, 3, null]);
+    expect(prependList.toString()).toStrictEqual(['one', 'two', 'three', null]);
+});
+
+test('can add a new value in the middle of a linked list', () => {
+    extraCreditList.appendValue(1);
+    extraCreditList.appendValue(3);
+    extraCreditList.insertAt('mid', 2);
+    expect(extraCreditList.atPosition(2).getValue()).toBe('mid');
 });
 
 
@@ -85,10 +97,18 @@ test('all functions handle an empty list', () => {
     expect(emptyList.pop()).toBe('List is empty');
     expect(emptyList.contains(3)).toBe('List is empty');
     expect(emptyList.find(5)).toBe('List is empty');
+    expect(emptyList.toString()).toBe('List is empty');
+    expect(otherExtraCreditList.insertAt(3, 3)).toBe('List was empty so appended a value.');
+    expect(otherExtraCreditList.getHead().getValue()).toBe(3);
 });
 
 
-
+/* Will not be incorporating DRY programming rules here rather finishing
+and moving on when im done figuring everything out, not worrying
+about efficient usage but probably should have kept DRY in mind
+as i was writing rather then going back over it and altering it to BECOME 
+DRY, for now rather focusing on getting it to work rather then
+making it pretty */
 
 /* when i appended 'one' in prevous test they are all in a global appendList
 variable, i could make individual lists for each test but just keeping
